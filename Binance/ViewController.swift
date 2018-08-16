53da3ba633219d9fe86a88291361309c1bbf4acb
Samuel Rockson <you@yourdomain.com>
2018-08-16 16:07:53 +0800

    Initial Commit

---
+++ Binance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Binance
+//
+//  Created by Rock on 2018/8/16.
+//  Copyright © 2018 RockzAppStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
